656b226126bb4d0af620f32ff3865fb55d8701a9
sh3ifu <denysdatskov201@gmail.com>
2026-06-08 12:56:45 +0300

    chore: bump Solidity to 0.8.35, sync bridge submodule

--- ethereum/src/interfaces/IUtexoLZAdapter.sol
+++ ethereum/src/interfaces/IUtexoLZAdapter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.35;
 
 /// @title IUtexoLZAdapter
 /// @notice Bidirectional adapter between the Utexo `Bridge` and the LayerZero / USDT0 stack
